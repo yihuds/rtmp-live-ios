310102073fba2b83cf90acb5627019b6ea46e4c5
胡栋松 <hudongsong@meituan.com>
2017-02-13 20:05:27 +0800

    add play button

--- rtmp-live-ios/rtmp-live-ios/ViewController.m
+++ rtmp-live-ios/rtmp-live-ios/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *playBtnContainView;
+@property (weak, nonatomic) IBOutlet UIButton *pushStreamBtn;
+@property (weak, nonatomic) IBOutlet UIButton *orderPlayBtn;
+@property (weak, nonatomic) IBOutlet UIButton *livePlayBtn;
+
 @end
 
 @implementation ViewController
@@ -17,11 +22,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)playButtonOnClick:(UIButton*)sender {
+    for (int tagIndex=1; tagIndex<=3; tagIndex++) {
+        UIButton* playButton = (UIButton*)[self.playBtnContainView viewWithTag:tagIndex];
+        if (tagIndex == sender.tag) {
+            [sender setBackgroundColor:[UIColor blueColor]];
+        } else {
+            [playButton setBackgroundColor:[UIColor colorWithRed:231/255.0 green:231/255.0 blue:231/255.0 alpha:1.0]];
+        }
+    }
+}
+
+- (void)initView {
+    
+}
+
+
 
 @end
