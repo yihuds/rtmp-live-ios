06b4ef925d695781560709b1505dc4217aedcec9
胡栋松 <hudongsong@meituan.com>
2017-02-26 19:53:26 +0800

    ui
    ui

--- rtmp-live-ios/rtmp-live-ios/ViewController.m
+++ rtmp-live-ios/rtmp-live-ios/ViewController.m
@@ -17,7 +17,9 @@
 @property (weak, nonatomic) IBOutlet UIButton   *orderPlayBtn;
 @property (weak, nonatomic) IBOutlet UIButton   *livePlayBtn;
 @property (weak, nonatomic) IBOutlet UIView     *videoView;
+@property (weak, nonatomic) IBOutlet UIButton *startOrStopBtn;
 
+@property (nonatomic, assign) NSInteger         currentSelectPlayIndex;
 
 @property (nonatomic, strong) AVCaptureSession           *session;
 @property (nonatomic, strong) dispatch_queue_t           videoQueue;
@@ -37,14 +39,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     [self initView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (IBAction)playButtonOnClick:(UIButton*)sender {
@@ -52,13 +52,29 @@
         UIButton* playButton = (UIButton*)[self.playBtnContainView viewWithTag:tagIndex];
         if (tagIndex == sender.tag) {
             [sender setBackgroundColor:[UIColor blueColor]];
+            _currentSelectPlayIndex = sender.tag;
         } else {
             [playButton setBackgroundColor:[UIColor colorWithRed:231/255.0 green:231/255.0 blue:231/255.0 alpha:1.0]];
         }
     }
 }
 
+- (IBAction)startOrStopOnClick:(id)sender {
+    if (_currentSelectPlayIndex == 0) {
+        
+    } else if (_currentSelectPlayIndex ==1) {
+        
+    } else if (_currentSelectPlayIndex == 2) {
+        
+    }
+}
+
 - (void)initView {
+    [self.pushStreamBtn sendActionsForControlEvents:UIControlEventTouchUpInside];
+    
+    self.startOrStopBtn.layer.cornerRadius = 4.0;
+    self.startOrStopBtn.layer.borderColor = [UIColor colorWithRed:102.0/255 green:172.0/255 blue:252.0/255 alpha:1.0].CGColor;
+    self.startOrStopBtn.layer.borderWidth = 1.0;
     
 }
 
